6f4f29a7d9a29deee3030b94eb94263f2f84202a
krishjx <109218551+krishjx@users.noreply.github.com>
2026-08-26 16:29:49 +0530

    Time: 0 ms (100%), Space: 11.5 MB (79.22%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -13,8 +13,6 @@ public:
         }
          i=0,j=nums.size()-1;
          while(i<j){
-            // if(nums[i]==0 || nums[i]==1){i++;}
-
              if(nums[i]>nums[j]){ 
                 nums[i]=nums[i]+nums[j];
                 nums[j]=nums[i]-nums[j];
@@ -23,25 +21,6 @@ public:
             if(nums[i]==0 || nums[i]==1){i++;}
              if(nums[j]==2){j--;}
         }
-        // while(i<j){
-        //     if(nums[i]>nums[j]){
-        //         nums[i]=nums[i]+nums[j];
-        //         nums[j]=nums[i]-nums[j];
-        //         nums[i]=nums[i]-nums[j];
-        //     }
-        //     if((nums[i]==nums[j]) &&(i+1==j || i==j)){
-        //         break;
-        //     }
-        //     if(nums[i]==nums[j]){
-        //         nums[i]=nums[i]+nums[i+1];
-        //         nums[i+1]=nums[i]-nums[i+1];
-        //         nums[i]=nums[i]-nums[i+1];
-                
-        //     }
-             
-        //     if(nums[i]==0){i++;}
-        //     if(nums[j]==2){j--;}
-            
-        // }
+        
     }
 };
