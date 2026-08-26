af4b24092ef91d0a6c6bfafe894302836c9327a2
krishjx <109218551+krishjx@users.noreply.github.com>
2026-08-26 12:57:45 +0530

    Time: 0 ms (100%), Space: 8.4 MB (83.55%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int a=0;
+        int long long sum=0,s=1;
+        if(x<0){
+            s=-1;
+            x=s*x;
+        }
+        while(x>0){
+            a=x%10;
+            sum=sum*10+a;
+            x=x/10;
+            if(s*sum>INT_MAX || INT_MIN>s*sum){return 0;}
+        }
+        
+        return s*sum;
+
+    }
+};
