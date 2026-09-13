886418c42d975ef31fb93360c151017273b8510f
krishjx <109218551+krishjx@users.noreply.github.com>
2026-09-13 18:03:51 +0530

    Time: 0 ms (100%), Space: 14.6 MB (84.86%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -4,19 +4,16 @@ public:
         int n = 3;
         int r = mat.size(), c = mat[0].size(), flag = 8;
         while (n >= 0) {
-
-           
-
-            // transposing
+            // transposing and reversing
             for (int i = 0; i < r; i++) {
                 for (int j = i + 1; j < c; j++) {
                     int temp = mat[i][j];
                     mat[i][j] = mat[j][i];
                     mat[j][i] = temp;
                 }
-            }
-            // reversing a transposed row
-            for (int i = 0; i < r; i++) {
+           
+            // reversing a transposed row at same time i not exit the main for loop
+
                 int b = c - 1, a = 0;
                 while (a < b) {
                     int temp = mat[i][a];
@@ -27,24 +24,19 @@ public:
                 }
             }
 
+            // checking both are same or not
             for (int i = 0; i < r; i++) {
                 flag = 8;
                 for (int j = 0; j < c; j++) {
-                    if (mat[i][j] != target[i][j]) {
-                
-
-                    flag = 0;
-                }}
-                if (flag == 0) {
-                    break;
-                }
+                    if (mat[i][j] != target[i][j]){flag = 0;}
             }
-            if (flag == 8) {
+                if (flag == 0){break;}
+            }
+            if (flag == 8){
                 return 1;
             }
             n--;
         }
-
         return 0;
     }
 };
